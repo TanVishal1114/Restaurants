ef61b182e69f3a714420605ad163e399711fd7eb
Tanvi Mehta <tanvi.n.raval@gmail.com>
2026-02-11 13:45:25 -0500

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowedPageSizes = new[] { 5, 10, 20, 50 };
+    public GetAllRestaurantQueryValidator()
+    {
+        RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(1);
+        RuleFor(x => x.PageSize).Must(value => allowedPageSizes.Contains(value))
+                                   .WithMessage($"Page size must be in [{string.Join(",", allowedPageSizes)}]");
+    }
+}
